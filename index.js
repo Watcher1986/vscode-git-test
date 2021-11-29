88394c839b99d9a150e193b224d9e9609a3b97cd
Denis <Deniss2007@gmail.com>
2021-11-29 20:11:48 +0200

    practice with pull request

---
+++ index.js
@@ -0,0 +1,13 @@
+const fetchData = async () => {
+  try {
+    const res = await fetch('');
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+    }
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+    alert(data);
+  }
+};
